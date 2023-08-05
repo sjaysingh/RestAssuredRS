6abb571404ccb8130a2e54225a1cd87939ecc46f
sjaysingh <sjsingh785@gmail.com>
2023-08-06 00:01:57 +0530

    reading data from external json file

--- src/main/java/org/example/main/DynamicJson.java
+++ src/main/java/org/example/main/DynamicJson.java
@@ -7,6 +7,10 @@ import org.example.lib.ResuableMethods;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.given;
 
 public class DynamicJson {
@@ -52,5 +56,25 @@ public class DynamicJson {
     @DataProvider(name = "booksDataSame")
     public Object[][] getData(){
         return new Object[][] {{"21","yud6a"},{"7","98h2k"}};
+    }
+
+    public void addBookFromFile(String aisle, String isbn) throws IOException {
+        RestAssured.baseURI = "http://216.10.245.166";
+
+        String bodyData = new String(Files.readAllBytes(Paths.get("/Users/talismansjay/Desktop/RestAssured/Udemy - Rahul Shetty Rest API Automation Testing/addPlace.json")));
+
+        String response = given().header("content-type","application/json").
+                body(bodyData).
+                when().
+                post("/Library/AddBook.php").
+                then().log().all().assertThat().statusCode(404).
+                extract().response().toString();
+
+        JsonPath js = ResuableMethods.rawToJson(response);
+        String bookId = js.getString("ID");
+        System.out.println(bookId);
+
+        System.out.printf(js.prettify());
+
     }
 }
